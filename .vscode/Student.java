3cf2d538e6c8aa34cd24371a4a4f74b710e6f68f
PrasanthNaidu83 <prasanthnaidu9848@gmail.com>
2026-02-02 18:09:55 +0530

    Add Student and Second classes with main method for testing

---
+++ .vscode/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+  int x = 5;
+}
+class Second {
+  public static void main(String[] args) {
+    Student myObj = new Student();
+    System.out.println(myObj.x);
+  }
+}
---
+++ .vscode/second.java
@@ -0,0 +1,6 @@
+class Second {
+  public static void main(String[] args) {
+    Second myObj = new Second();
+    System.out.println(myObj.x);
+  }
+}
